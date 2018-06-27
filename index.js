d195a20bac01e4088a572ace1e0a424c2b58a929
Chribtoper <xcstolfax@gmail.com>
2018-06-27 23:22:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,14 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
+<<<<<<< HEAD
   if (string === string.toLowerCase()) return "I can\'t hear you!"
   if (string === "I love you, Grandma.") return "I love you, too."
   else return "YES INDEED!"
 }
 
 
+=======
+  if (string === string.whisper) return "I can\'t hear you!"
+}
+>>>>>>> 846562d65738deb5978c4dc51444c81e675ce0fc
